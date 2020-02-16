81f5b66a846693602d48251da84462e6a354015a
linhphamthi97 <linh.phamthi@balliol.ox.ac.uk>
2020-02-16 16:26:39 +0000

    Iterative reference setting

--- set_reference_state.jl
+++ set_reference_state.jl
@@ -6,9 +6,10 @@ Need to use package https://github.com/KristofferC/NearestNeighbors.jl before ru
 =#
 
 using Ipopt, JuMP, LinearAlgebra, NearestNeighbors, DelimitedFiles
+include("settings.jl")
 
 function set_reference_state(x_current)
-    x_ref = Vector{Float64}(undef, 4)
+    x_ref = Array{Float64}(undef, 4, settings.horizon_length)
 
     # Choosing the closest centerline point to current location and use that as reference
         # Load data from csv file, skip first row (header)
@@ -27,14 +28,19 @@ function set_reference_state(x_current)
         centerline_point_index, centerline_distance = knn(tree, x_current[1:2], 1, false)
         centerline_point = data[:,centerline_point_index]
 
-        println("Centerline point: ", centerline_point)
+        # Set reference state
+        x_ref[1,1] = centerline_point[1]        # x coordinate
+        x_ref[2,1] = centerline_point[2]        # y coordinate
 
+        # Query next points, within distance v*delta_t away
+        for i=2:settings.horizon_length
+            idx = inrange(tree, x_ref[1:2,i-1], x_current[4] * settings.delta_t, false)
+            x_ref[1:2,i] = data[:, maximum(idx)]
+        end
 
     # Set reference state
-    x_ref[1] = centerline_point[1]        # x coordinate
-    x_ref[2] = centerline_point[2]        # y coordinate
-    x_ref[3] = 0                          # angular velocity
-    x_ref[4] = 1                          # velocity of centre of mass
+    x_ref[3, :] .= 0                          # angular velocity
+    x_ref[4, :] .= 1                          # velocity of centre of mass
 
     return x_ref
 
--- settings.jl
+++ settings.jl
@@ -5,16 +5,18 @@ created on: 02.02.2020
 This file contains all of the settings to change about the simulation or the model.
 =#
 
-module Settings
-export u_initial, x_initial, horizon_length
+module settings
+export u_initial, x_initial, horizon_length, delta_t
 # Simulation parameters
-
+    delta_t = 0.1
 
 # Initial conditions
     u_initial = [0, 0]
     x_initial = [0, -3, 0, 1]
 
-# Control model parameters
+# Control model (bicycle model) parameters
     horizon_length = 10
+
+    
 
 end # module
