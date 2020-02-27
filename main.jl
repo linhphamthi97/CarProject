7eb89ee3ec4ba6f6e27a1c572fab6a7b5644941d
Michael <michael@garstka.org>
2020-02-27 15:13:53 +0000

    Save the input and state history
    **For debugging:**
    - Plot the racetrack and the state history (x,y) of the car
    after the simulation has finished
    - For now I uncommented the animation

--- main.jl
+++ main.jl
@@ -13,6 +13,8 @@ The structure of the program is as follows:
 =#
 
 using Plots, Ipopt, JuMP
+using CSV, DataFrames # to plot the racetrack
+
 include("run_mpc.jl")
 include("set_reference_state.jl")
 include("simulation.jl")
@@ -23,18 +25,25 @@ function main()
     gr()
 
     # Intialise parameters
+    N_sim = 20
     u = settings.u_initial
     x = settings.x_initial
     x_ref = Vector{Float64}(undef, 4)
     x_plan = repeat(settings.x_initial, 1, 2)
     u_plan = Vector{Float64}(undef, 2)
 
+
+    # just keep the history for debugging
+    x_hist = zeros(Float64, N_sim + 1, 4)
+    x_hist[1, :] = x
+    u_hist = zeros(Float64, N_sim, 2)
     # Create an animation
-    anim = @animate for i in 1:1
+    #anim = @animate for i in 1:1
+    for i in 1:N_sim
         println("Timestep ", i)
 
         # Plot the current position and the track
-        plot([x[1]], [x[2]], marker=(:hex, 10), xlim=(-2, 8), ylim=(-4, 2))
+        #plot([x[1]], [x[2]], marker=(:hex, 10), xlim=(-2, 8), ylim=(-4, 2))
 
         # Set reference state
         x_ref = set_reference_state(x)
@@ -45,10 +54,14 @@ function main()
         x_plan, u = run_mpc(x_ref, x, u, x_plan, settings.horizon_length)
 
         # Draw the planned future states from the MPC optimization
-        plot!(x_plan[1, :], x_plan[2, :], linewidth=5)
+        #plot!(x_plan[1, :], x_plan[2, :], linewidth=5)
 
         # Apply the planned inputs and simulate one step in time
         x = simulation(x, u)
+        # save the history of states and inputs
+        x_hist[i + 1, :] = x
+        u_hist[i, :] = u
+
         # println("x after simulation= ", x)
         # println(" ")
         # println(" ")
@@ -56,8 +69,12 @@ function main()
     end
 
     # Save animation as a gif file
-    gif(anim, "/mpc.gif", fps = 10)
-
+    #gif(anim, "/mpc.gif", fps = 10)
+    return x_plan, x_ref, u_plan, x_hist, u_hist
 end
 
-main()
+x_plan, x_ref, u_plan, x_hist, u_hist = main()
+# plot the race track and states
+track2500 = CSV.read("3yp_track2500.csv");
+plot(track2500.x, track2500.y, label = "racetrack-centerline")
+plot!(x_hist[:, 1], x_hist[:, 2], label = "car trajectory")
